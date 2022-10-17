6537f9749aaf6d1916b31c1777178a4adbf4b4de
max-piskunov <psknoff@yandex.ru>
2022-10-17 16:22:09 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -14,5 +14,47 @@ function checkMaxString(str, maxStr) {
   }
 }
 
-getRandomNumber(4, 7);
+
 checkMaxString('Hi', 'Hello');
+
+
+let photoDescription = ['Новое фото', 'Еще одно', 'Выходные', 'Без комментариев',
+ 'Как-то так', 'Начало', 'День первый', 'День второй', 'Отлично!', 'Такое', 'Вот так',
+  'Интересно', 'Это правда', 'Оставлю здесь'
+];
+
+let likesCount = {
+  min: 15,
+  max: 200
+};
+
+let commentsCount = {
+  min: 0,
+  max: 200
+};
+
+const OBJECT_COUNT = 25;
+
+let getRandomArrayElement = (elements) => elements
+[getRandomNumber(0, elements.length - 1)];
+
+function arrayShuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+
+let createPhoto = (objectIndex) => ({
+  id: objectIndex,
+  url: `photos/${objectIndex}.jpg`,
+  description: getRandomArrayElement(photoDescription),
+  likes: getRandomNumber(likesCount.min, likesCount.max),
+  comments: getRandomNumber(commentsCount.min, commentsCount.max)
+});
+
+//const similarObjects = Array.from({length: OBJECT_COUNT}, createPhoto);
+
+const similarObjects = Array.from({length: OBJECT_COUNT}, (_, objectIndex) => createPhoto(objectIndex + 1));
+
+arrayShuffle(similarObjects);
+
+console.log(similarObjects);
