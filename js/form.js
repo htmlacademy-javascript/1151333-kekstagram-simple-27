961c0369fadc04ff44e834fdeff7914658302190
max-piskunov <psknoff@yandex.ru>
2022-11-17 15:21:35 +0300

    Надо подкачаться

--- js/form.js
+++ js/form.js
@@ -1,4 +1,19 @@
+import {showSuccessMessage} from './success-message-popup.js';
+import {showErrorMessage} from './error-message-popup.js';
+import {sendData} from './api.js';
+
 const form = document.querySelector('.img-upload__form');
+const submitButton = form.querySelector('.img-upload__submit');
+
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Публикую...';
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__text',
@@ -6,12 +21,27 @@ const pristine = new Pristine(form, {
   errorTextClass: 'text__description__error-text',
 });
 
-
-form.addEventListener('submit', (evt) => {
-  const isValid = pristine.validate();
-
-  if (!isValid) {
+const setUserFormSubmit = (onSuccess) => {
+  form.addEventListener('submit', (evt) => {
     evt.preventDefault();
-  }
+    const isValid = pristine.validate();
 
-});
+    if (isValid) {
+      blockSubmitButton();
+      sendData(
+        () => {
+          onSuccess();
+          showSuccessMessage();
+          unblockSubmitButton();
+        },
+        () => {
+          showErrorMessage();
+          unblockSubmitButton();
+        },
+        new FormData(evt.target),
+      );
+    }
+  });
+};
+
+export {setUserFormSubmit};
